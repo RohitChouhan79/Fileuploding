cd38addebfce07dac0d116c88f710487cec56ca1
Rohit Chouhan <Rohitchouhan201090@gmail.com>
2023-11-05 23:06:53 +0530

    some comments added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 
+// we call the multer file and use single middlewere file
 var uploads = require("../utils/multer").single("Avtar");
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,7 +10,9 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.post("/uploads", uploads, function (req, res, next) {
+// uploading page 
+router.post("/uploads", function (req, res, next) {
+  // upload is midddleware
   uploads(req, res, function (err) {
     if (err) throw err;
     res.json({ body: req.body, file: req.file })
--- utils/multer.js
+++ utils/multer.js
@@ -1,10 +1,12 @@
-const multer=require("multer")
+const multer=require("multer") 
 const path=require("path")
 const storage=multer.diskStorage({
+    // destination helps to find the destination of uploading file
     destination: function(req,file,cb){
         console.log(file);
         cb(null,"./public/uploads");
     },
+    // this file name will give a unique file name
     filename:function(req,file,cb){
         let modifyfiedfilename=file.fieldname+"-"+Date.now()+path.extname(file.originalname)
         cb(null,modifyfiedfilename)
@@ -13,6 +15,7 @@ const storage=multer.diskStorage({
 
 const uploads=multer({
     storage:storage,
+    // filefilter will check file extention then file will will uploades
     fileFilter: function (req, file, cb) {
         let filetypes = /jpeg|jpg|png|gif|webp|svg/;
         let mimetype = filetypes.test(file.mimetype);
